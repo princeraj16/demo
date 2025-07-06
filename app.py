faecab5cce06f037815c9675ed80fceeac4fe092
princeraj16 <princerajsharma857@gmail.com>
2025-07-07 02:16:24 +0530

    first

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello world")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/name')
+def greet():
+    return "🚀 Hello from Flask on Red Hat UBI!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
